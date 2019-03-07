55dbbafccb9a65619b9fc1e693566b89d4523bd9
Cesarm123 <48258779+Cesarm123@users.noreply.github.com>
2019-03-07 10:39:49 -0800

    Update main.c

--- main.c
+++ main.c
@@ -8,3 +8,25 @@ int main()
   }
   return 0;
 }
+int main()
+{
+  int i;
+  for (i = 0; i < 10; i++)
+  {
+    printf("Hello 
+           Holberton\n");
+  }
+  return 0;
+}
+int main()
+{
+  int i;
+  for (i = 0; i < 10; i++)
+  {
+    printf("Hello 
+           Holberton\n");
+  }
+  return 0;
+}
+int main()
+{           
